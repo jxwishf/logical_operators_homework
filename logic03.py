017b91836edd56148c63470a5295cf1435f900f8
ja <jaxa42421@gmail.com>
2025-06-10 14:58:23 +0500

    task3

--- logic03.py
+++ logic03.py
@@ -1,10 +1,5 @@
-def main(a,b):
-    """
-    Given two integers a, b,  check the following statement "Each of the numbers 'a' and 'b' is negative".
-    Args:
-        a(int): parameter a
-        b(int): parameter b
-    Returns:
-        bool: answer
-    """
-    return
+def fu(a, b):
+    return a < 0 and b < 0
+
+print(fu(14,-53))
+
