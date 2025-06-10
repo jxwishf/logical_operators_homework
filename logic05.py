467a388d343b25423ade9f63bd644e97dfaaa04d
ja <jaxa42421@gmail.com>
2025-06-10 15:02:48 +0500

    task5

--- logic05.py
+++ logic05.py
@@ -1,10 +1,3 @@
 def main(a,b):
-    """
-    Given two integers a, b,  check the following statement "Each of the numbers 'a' and 'b' is odd".
-    Args:
-        a(int): parameter a
-        b(int): parameter b
-    Returns:
-        bool: answer
-    """
+    return a % 2 != 0 and b % 2 != 0
     return
