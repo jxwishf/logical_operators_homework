e66a1f591a681aa338bf0decad7dd0ac9565858b
ja <jaxa42421@gmail.com>
2025-06-10 14:56:04 +0500

    task2

--- logic02.py
+++ logic02.py
@@ -1,10 +1,3 @@
-def main(a,b):
-    """
-    Given two integers a, b,  check the following statement "Each of the numbers 'a' and 'b' is positive".
-    Args:
-        a(int): parameter a
-        b(int): parameter b
-    Returns:
-        bool: answer
-    """
-    return
+def FU(a, b):
+    return a > 0 and b > 0
+print(FU(11,45))
